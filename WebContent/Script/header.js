11197f5c1e2549b884733d131ba1a2a515d0d78b
Pramod Jalindar Patsalge <35419762+iampramodpatsalge@users.noreply.github.com>
2018-04-13 05:18:21 +0530

    added Project

---
+++ WebContent/Script/header.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function quickCall() {
+    alert("call:1800 4433 2211");
+}
+
+function openPage(pageName,elmnt,color) 
+{
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+}
---
+++ WebContent/Script/route.js
@@ -0,0 +1,26 @@
+
+
+
+app.config(function($routeProvider)
+		{
+				$routeProvider.when('/Careers',
+						{
+								templateUrl:'partials/careers.html'
+						});
+				$routeProvider.when('/OurSkills',
+						{
+								templateUrl:'partials/ourskills.html'
+						});
+				$routeProvider.when('/AboutUs',
+						{
+								templateUrl:'partials/ourskills.html'
+						});
+				$routeProvider.when('/OurTeam',
+						{
+								templateUrl:'partials/ourteam.html'
+						});
+				$routeProvider.when('/ContactUs',
+						{
+								templateUrl:'partials/contactus.html'
+						});
+		})
